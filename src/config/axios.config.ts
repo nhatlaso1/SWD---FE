4e1af2e26afcf48568031070a4376b942de237a0
HighlandCat29 <minecaftlichking@gmail.com>
2025-03-15 22:58:34 +0700

    update axios

--- src/config/axios.config.ts
+++ src/config/axios.config.ts
@@ -3,10 +3,8 @@ import helpers from '../helpers';
 
 const baseURL =
   process.env.NODE_ENV === 'production'
-    ? 'https://test/'
-    : 'http://localhost:8888/';
-
-const token = helpers.cookie_get('AT');
+    ? 'https://swd392-be-web-hsdcamfearhyh8ga.eastasia-01.azurewebsites.net/'
+    : 'http://localhost:8080/';
 
 const onRequestSuccess = (config: any) => {
   config.headers['Authorization'] = `Bearer ${helpers.cookie_get('AT')}`;
@@ -37,200 +35,104 @@ axios.interceptors.response.use(onResponseSuccess, onResponseError);
 axios.defaults.baseURL = baseURL;
 
 var BaseRequest = {
-  Get: async <T = any>(url: string): Promise<T> => {
+  Get: async <T = any>(url: string) => {
     try {
-      const response: AxiosResponse<T> = await axios.get<T>(url);
-      return response.data;
+      const response: any = await axios.get<T>(url);
+      return response; // Trả về toàn bộ response thay vì chỉ response.data
     } catch (err) {
       console.error('GET Error:', err);
       throw err;
     }
   },
 
-  Post: async <T = any, D = any>(url: string, data?: D): Promise<T> => {
+  Post: async <T = any, D = any>(url: string, data?: D) => {
     try {
-      const response: AxiosResponse<T> = await axios.post<T>(url, data);
-      return response.data;
+      const response: any = await axios.post<T>(url, data);
+      return response; // Trả về toàn bộ response
     } catch (err) {
       console.error('POST Error:', err);
       throw err;
     }
   },
 
-  Put: async <T = any, D = any>(url: string, data?: D): Promise<T> => {
+  Put: async <T = any, D = any>(
+    url: string,
+    data?: D
+  ): Promise<AxiosResponse<T>> => {
     try {
-      const response: AxiosResponse<T> = await axios.put<T>(url, data);
-      return response.data;
+      const response: any = await axios.put<T>(url, data);
+      return response;
     } catch (err) {
       console.error('PUT Error:', err);
       throw err;
     }
   },
 
-  Patch: async <T = any, D = any>(url: string, data?: D): Promise<T> => {
+  Patch: async <T = any, D = any>(url: string, data?: D) => {
     try {
-      const response: AxiosResponse<T> = await axios.patch<T>(url, data);
-      return response.data;
+      const response: any = await axios.patch<T>(url, data);
+      return response;
     } catch (err) {
       console.error('PATCH Error:', err);
       throw err;
     }
   },
 
-  Delete: async <T = any>(url: string): Promise<T> => {
+  Delete: async <T = any>(url: string): Promise<AxiosResponse<T>> => {
     try {
       const response: AxiosResponse<T> = await axios.delete<T>(url);
-      return response.data;
+      return response;
     } catch (err) {
       console.error('DELETE Error:', err);
       throw err;
-    }
-  },
-  UploadDatFile: async (model: any) => {
-    try {
-      const formData = new FormData();
-      for (const key in model) {
-        formData.append(key, model[key]);
-      }
-      const response = await axios.post(`/upload/upload-dat`, formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-          Authorization: `Bearer ${token}`
-        }
-      });
-      return response.data;
-    } catch (err) {
-      console.error('Error in UploadDatFile request:', err);
-      throw err;
-    }
-  },
-
-  UploadJSONCheckScore: async (model: any) => {
-    try {
-      const formData = new FormData();
-      for (const key in model) {
-        formData.append(key, model[key]);
-      }
-      const response = await axios.post(
-        `/upload/upload-json-check-score`,
-        formData,
-        {
-          headers: {
-            'Content-Type': 'multipart/form-data',
-            Authorization: `Bearer ${token}`
-          }
-        }
-      );
-      return response.data;
-    } catch (err) {
-      console.error('Error in UploadJSONCheckScore request:', err);
-      throw err;
-    }
-  },
-
-  UploadJSONQuiz: async (model: any) => {
-    try {
-      const formData = new FormData();
-      for (const key in model) {
-        formData.append(key, model[key]);
-      }
-      const response = await axios.post(`/upload/upload-json-quiz`, formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-          Authorization: `Bearer ${token}`
-        }
-      });
-      return response.data;
-    } catch (err) {
-      console.error('Error in UploadJsonQuiz request:', err);
-      throw err;
-    }
-  },
-
-  UploadExamZip: async (model: any) => {
-    try {
-      const formData = new FormData();
-      for (const key in model) {
-        formData.append(key, model[key]);
-      }
-      const response = await axios.post(`/upload/upload-exam-zip`, formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-          Authorization: `Bearer ${token}`
-        }
-      });
-      return response.data;
-    } catch (err) {
-      console.error('Error in UploadExamZip request:', err);
-      throw err;
-    }
-  },
-
-  uploadSingleImage: async (file: File) => {
-    try {
-      const formData = new FormData();
-      formData.append('file', file);
-      const response = await axios.post(
-        '/upload/upload-image-local',
-        formData,
-        {
-          headers: {
-            'Content-Type': 'multipart/form-data',
-            Authorization: `Bearer ${token}`
-          }
-        }
-      );
-      return response.data;
-    } catch (error) {
-      console.error('Error uploading image:', error);
-      throw error;
     }
   }
 };
 
 var BaseRequestV2 = {
-  Get: async <T = any>(url: string): Promise<[any, T | null]> => {
+  Get: async <T = any>(
+    url: string
+  ): Promise<[any, AxiosResponse<T> | null]> => {
     try {
-      const res: any = await axios.get(url);
-      if (res && res.success) {
-        return [null, res.data];
-      }
-      return [res.message || 'Unknown error occurred', null];
+      const res: AxiosResponse<T> = await axios.get(url);
+      return [null, res]; // Trả về toàn bộ response thay vì chỉ res.data
     } catch (err: any) {
-      return [err?.data?.message || err.message || 'Request failed', null];
+      return [err?.response || err, null]; // Trả về toàn bộ lỗi response nếu có
     }
   },
-  Post: async <T = any>(url: string, data?: any): Promise<[any, T | null]> => {
+
+  Post: async <T = any>(
+    url: string,
+    data?: any
+  ): Promise<[any, AxiosResponse<T> | null]> => {
     try {
-      const res: any = await axios.post(url, data);
-      if (res && res.success) {
-        return [null, res.data];
-      }
-      return [res.message || 'Unknown error occurred', null];
+      const res: AxiosResponse<T> = await axios.post(url, data);
+      return [null, res];
     } catch (err: any) {
-      return [err?.data?.message || err.message || 'Request failed', null];
+      return [err?.response || err, null];
     }
   },
-  Put: async <T = any>(url: string, data: any): Promise<[any, T | null]> => {
+
+  Put: async <T = any>(
+    url: string,
+    data: any
+  ): Promise<[any, AxiosResponse<T> | null]> => {
     try {
-      const res: any = await axios.put(url, data);
-      if (res && res.success) {
-        return [null, res.data];
-      }
-      return [res.message || 'Unknown error occurred', null];
+      const res: AxiosResponse<T> = await axios.put(url, data);
+      return [null, res];
     } catch (err: any) {
-      return [err?.data?.message || err.message || 'Request failed', null];
+      return [err?.response || err, null];
     }
   },
-  Delete: async <T = any>(url: string): Promise<[any, T | null]> => {
+
+  Delete: async <T = any>(
+    url: string
+  ): Promise<[any, AxiosResponse<T> | null]> => {
     try {
-      const res: any = await axios.delete(url);
-      if (res && res.success) {
-        return [null, res.data];
-      }
-      return [res.message || 'Unknown error occurred', null];
+      const res: AxiosResponse<T> = await axios.delete(url);
+      return [null, res];
     } catch (err: any) {
-      return [err?.data?.message || err.message || 'Request failed', null];
+      return [err?.response || err, null];
     }
   }
 };
